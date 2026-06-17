aedb0e2a2aca2b18c5fcf5c4f007721c1677f4a6
Dshehara <dilmirathnayake431@gmail.com>
2026-06-17 14:47:14 +0530

    Day 1: Hello World - my first C# program

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello , my name is Dilmi !");
+Console.WriteLine("I am a software Engineering Student.");
+Console.WriteLine("I am learning C# in 30 days.");

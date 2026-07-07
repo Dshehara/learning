43d5638944fd7b0db06e3728772836b5cb8df57c
Dshehara <dilmirathnayake431@gmail.com>
2026-07-07 14:44:32 +0530

    Day 2: variables, if/else, loops

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello , my name is Dilmi !");
-Console.WriteLine("I am a software Engineering Student.");
-Console.WriteLine("I am learning C# in 30 days.");
+﻿for(int i = 1; i <= 20; i++)
+{
+    if(i % 5 == 0)
+    {
+        Console.WriteLine("buzz");
+    }else
+    {
+        Console.WriteLine(i);
+    }
+}
--- obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aedb0e2a2aca2b18c5fcf5c4f007721c1677f4a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

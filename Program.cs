5428b83d278124ba7a0d41e0a2a7a17a38c1720b
Dshehara <dilmirathnayake431@gmail.com>
2026-07-08 12:37:57 +0530

    Day 3: methods , classes, objects, BankAccount project

--- Program.cs
+++ Program.cs
@@ -1,10 +1,49 @@
-﻿for(int i = 1; i <= 20; i++)
+﻿// bank account
+BankAccount acc1 = new BankAccount("Dilmi", 1000);
+acc1.PrintBalance();
+acc1.Deposit(500);
+acc1.Withdraw(200);
+acc1.Withdraw(2000);
+acc1.PrintBalance();
+
+
+BankAccount acc2 = new BankAccount("Kasun", 500);
+acc2.Deposit(100);
+acc2.PrintBalance();
+
+class BankAccount
 {
-    if(i % 5 == 0)
+    public string Owner;
+    public double Balance;
+
+    public BankAccount(string owner, double balance)
     {
-        Console.WriteLine("buzz");
-    }else
+        Owner = owner;
+        Balance = balance;
+    }
+
+    public void Deposit(double amount)
     {
-        Console.WriteLine(i);
+        Balance += amount ;
+        Console.WriteLine("Deposited : " + amount + ". New balance : " + Balance);
+    }
+
+    public void Withdraw(double amount)
+    {
+
+        if (amount > Balance)
+        {
+            Console.WriteLine("Insufficient funds");
+        }
+        else
+        {
+            Balance -= amount;
+            Console.WriteLine("Withdraw  : " + amount + ". New balance : " + Balance);
+        }
+    }
+
+    public void PrintBalance()
+    {
+        Console.WriteLine("Owner : " + Owner + ", Balance: " + Balance);
     }
 }
--- obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aedb0e2a2aca2b18c5fcf5c4f007721c1677f4a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43d5638944fd7b0db06e3728772836b5cb8df57c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
